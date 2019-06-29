5f5b618be4ac6a942ad7a789e83d874616e98909
A_suozhang <A_suozhang@outlook.com>
2019-06-29 09:06:46 +0800

    Tested Basics Of ESP32

--- ESP32/ESP32_Test/esp32_test.ino
+++ ESP32/ESP32_Test/esp32_test.ino
@@ -1,17 +1,14 @@
 
-int i = 0;
-
-void setup(){
-    pinMode(25,OUTPUT);
-    Serial.begin(115200);
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(25, OUTPUT);
 }
 
-void loop(){
-    Serial.println(i);
-    i++;
-    digitalWrite(25,HIGH);
-    delay(500);
-    digitalWrite(25,LOW);
-    delay(500);
-
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(25, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(200);                       // wait for a second
+  digitalWrite(25, LOW);    // turn the LED off by making the voltage LOW
+  delay(200);                       // wait for a second
 }
